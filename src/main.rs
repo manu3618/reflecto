2f9347b5e2d400a2daf27ef98ef659938ad26e5a
manu3618 <manu+github@hbrt.eu>
2024-05-03 16:38:34 +0200

    Add JSON serializer for date

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use chrono::DateTime;
+use chrono::Utc;
 use serde::Deserialize;
 
 static MIRROR_STATUS_URL: &str = "https://archlinux.org/mirrors/status/json";
@@ -6,6 +8,7 @@ static MIRROR_STATUS_URL: &str = "https://archlinux.org/mirrors/status/json";
 struct MirrorList {
     urls: Vec<Mirror>,
 }
+
 #[derive(Debug, Default, Deserialize)]
 struct Mirror {
     /// url
@@ -13,8 +16,29 @@ struct Mirror {
     protocol: Protocol,
     score: f64,
     delay: Option<f64>,
+    #[serde(with = "parse_date")]
+    last_sync: DateTime<Utc>,
     /// detailed url
     details: String,
+}
+
+/// home made implementation of serde deserializer for dates
+mod parse_date {
+    use chrono::DateTime;
+    use chrono::Utc;
+    use serde::{self, Deserialize, Deserializer};
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s: String = String::deserialize(deserializer)?;
+        if let Ok(d) = DateTime::parse_from_rfc3339(&s) {
+            Ok(d.into())
+        } else {
+            todo!()
+        }
+    }
 }
 
 impl Mirror {
@@ -26,6 +50,7 @@ impl Mirror {
 enum Protocol {
     #[default]
     HTTPS,
+    HTTP,
     RSYNC,
 }
 
